8745b75dd937def5481320bad9bed0b062536948
wenghuichen <wenghuichen@eleadernet.com>
2026-08-21 07:14:43 +0800

    2023 作品展

--- events/2023/exhibition/gallery-data.js
+++ events/2023/exhibition/gallery-data.js
@@ -1,287 +1,137 @@
 const eventGalleryData = {
-    year: "2016",
+    year: "2023",
 
-    title: "2016 作品展",
+    title: "2023 作品展",
 
-    subtitle: "Exhibition 2016",
+    subtitle: "Exhibition 2023",
 
     description:
-        "台灣國際拼布友好會 2016 年作品展紀錄。",
+        "台灣國際拼布友好會 2023 年作品展紀錄。",
 
     works: [
 
         {
-            image: "/events/2016/work-01.jpg",
+            image: "/events/2023/exhibition/work-01.jpg",
             title: "",
             artist: ""
         },
 
         {
-            image: "/events/2016/work-02.jpg",
+            image: "/events/2023/exhibition/work-02.jpg",
             title: "",
             artist: ""
         },
 
         {
-            image: "/events/2016/work-03.jpg",
+            image: "/events/2023/exhibition/work-03.jpg",
             title: "",
             artist: ""
         },
 
         {
-            image: "/events/2016/work-04.jpg",
+            image: "/events/2023/exhibition/work-04.jpg",
             title: "",
             artist: ""
         },
 
         {
-            image: "/events/2016/work-05.jpg",
+            image: "/events/2023/exhibition/work-05.jpg",
             title: "",
             artist: ""
         },
 
         {
-            image: "/events/2016/work-06.jpg",
+            image: "/events/2023/exhibition/work-06.jpg",
             title: "",
             artist: ""
         },
 
         {
-            image: "/events/2016/work-07.jpg",
+            image: "/events/2023/exhibition/work-07.jpg",
             title: "",
             artist: ""
         },
 
         {
-            image: "/events/2016/work-08.jpg",
+            image: "/events/2023/exhibition/work-08.jpg",
             title: "",
             artist: ""
         },
 
         {
-            image: "/events/2016/work-09.jpg",
+            image: "/events/2023/exhibition/work-09.jpg",
             title: "",
             artist: ""
         },
 
         {
-            image: "/events/2016/work-10.jpg",
+            image: "/events/2023/exhibition/work-10.jpg",
             title: "",
             artist: ""
         },
 
         {
-            image: "/events/2016/work-11.jpg",
+            image: "/events/2023/exhibition/work-11.jpg",
             title: "",
             artist: ""
         },
 
         {
-            image: "/events/2016/work-12.jpg",
+            image: "/events/2023/exhibition/work-12.jpg",
             title: "",
             artist: ""
         },
 
         {
-            image: "/events/2016/work-13.jpg",
+            image: "/events/2023/exhibition/work-13.jpg",
             title: "",
             artist: ""
         },
 
         {
-            image: "/events/2016/work-14.jpg",
+            image: "/events/2023/exhibition/work-14.jpg",
             title: "",
             artist: ""
         },
 
         {
-            image: "/events/2016/work-15.jpg",
+            image: "/events/2023/exhibition/work-15.jpg",
             title: "",
             artist: ""
         },
 
         {
-            image: "/events/2016/work-16.jpg",
+            image: "/events/2023/exhibition/work-16.jpg",
             title: "",
             artist: ""
         },
 
         {
-            image: "/events/2016/work-17.jpg",
+            image: "/events/2023/exhibition/work-17.jpg",
             title: "",
             artist: ""
         },
 
         {
-            image: "/events/2016/work-18.jpg",
+            image: "/events/2023/exhibition/work-18.jpg",
             title: "",
             artist: ""
         },
 
         {
-            image: "/events/2016/work-19.jpg",
+            image: "/events/2023/exhibition/work-19.jpg",
             title: "",
             artist: ""
         },
 
         {
-            image: "/events/2016/work-20.jpg",
+            image: "/events/2023/exhibition/work-20.jpg",
             title: "",
             artist: ""
         },
 
         {
-            image: "/events/2016/work-21.jpg",
-            title: "",
-            artist: ""
-        },
-
-        {
-            image: "/events/2016/work-22.jpg",
-            title: "",
-            artist: ""
-        },
-
-        {
-            image: "/events/2016/work-23.jpg",
-            title: "",
-            artist: ""
-        },
-
-        {
-            image: "/events/2016/work-24.jpg",
-            title: "",
-            artist: ""
-        },
-
-        {
-            image: "/events/2016/work-25.jpg",
-            title: "",
-            artist: ""
-        },
-
-        {
-            image: "/events/2016/work-26.jpg",
-            title: "",
-            artist: ""
-        },
-
-        {
-            image: "/events/2016/work-27.jpg",
-            title: "",
-            artist: ""
-        },
-
-        {
-            image: "/events/2016/work-28.jpg",
-            title: "",
-            artist: ""
-        },
-
-        {
-            image: "/events/2016/work-29.jpg",
-            title: "",
-            artist: ""
-        },
-
-        {
-            image: "/events/2016/work-30.jpg",
-            title: "",
-            artist: ""
-        },
-
-        {
-            image: "/events/2016/work-31.jpg",
-            title: "",
-            artist: ""
-        },
-
-        {
-            image: "/events/2016/work-32.jpg",
-            title: "",
-            artist: ""
-        },
-
-        {
-            image: "/events/2016/work-33.jpg",
-            title: "",
-            artist: ""
-        },
-
-        {
-            image: "/events/2016/work-34.jpg",
-            title: "",
-            artist: ""
-        },
-
-        {
-            image: "/events/2016/work-35.jpg",
-            title: "",
-            artist: ""
-        },
-
-        {
-            image: "/events/2016/work-36.jpg",
-            title: "",
-            artist: ""
-        },
-
-        {
-            image: "/events/2016/work-37.jpg",
-            title: "",
-            artist: ""
-        },
-
-        {
-            image: "/events/2016/work-38.jpg",
-            title: "",
-            artist: ""
-        },
-
-        {
-            image: "/events/2016/work-39.jpg",
-            title: "",
-            artist: ""
-        },
-
-        {
-            image: "/events/2016/work-40.jpg",
-            title: "",
-            artist: ""
-        },
-
-        {
-            image: "/events/2016/work-41.jpg",
-            title: "",
-            artist: ""
-        },
-
-        {
-            image: "/events/2016/work-42.jpg",
-            title: "",
-            artist: ""
-        },
-
-        {
-            image: "/events/2016/work-43.jpg",
-            title: "",
-            artist: ""
-        },
-
-        {
-            image: "/events/2016/work-44.jpg",
-            title: "",
-            artist: ""
-        },
-
-        {
-            image: "/events/2016/work-45.jpg",
-            title: "",
-            artist: ""
-        },
-
-        {
-            image: "/events/2016/work-46.jpg",
+            image: "/events/2023/exhibition/work-21.jpg",
             title: "",
             artist: ""
         }
