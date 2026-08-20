5f400c978ff6460b9c4c7931a512337f6d2e0268
wenghuichen <wenghuichen@eleadernet.com>
2026-08-20 23:22:57 +0800

    新增 2017 2018 作品展

---
+++ events/2017/gallery-data.js
@@ -0,0 +1,314 @@
+const eventGalleryData = {
+    year: "2017",
+
+    title: "2017 作品展",
+
+    subtitle: "Exhibition 2017",
+
+    description:
+        "台灣國際拼布友好會 2017 年作品展紀錄。",
+
+    works: [
+
+        {
+            image: "/events/2017/work-01.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2017/work-02.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2017/work-03.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2017/work-04.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2017/work-05.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2017/work-06.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2017/work-07.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2017/work-08.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2017/work-09.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2017/work-10.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2017/work-11.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2017/work-12.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2017/work-13.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2017/work-14.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2017/work-15.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2017/work-16.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2017/work-17.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2017/work-18.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2017/work-19.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2017/work-20.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2017/work-21.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2017/work-22.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2017/work-23.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2017/work-24.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2017/work-25.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2017/work-26.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2017/work-27.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2017/work-28.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2017/work-29.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2017/work-30.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2017/work-31.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2017/work-32.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2017/work-33.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2017/work-34.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2017/work-35.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2017/work-36.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2017/work-37.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2017/work-38.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2017/work-39.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2017/work-40.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2017/work-41.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2017/work-42.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2017/work-43.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2017/work-44.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2017/work-45.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2017/work-46.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2017/work-47.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2017/work-48.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2017/work-49.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2017/work-50.jpg",
+            title: "",
+            artist: ""
+        }
+
+    ]
+};
---
+++ events/2018/gallery-data.js
@@ -0,0 +1,373 @@
+const eventGalleryData = {
+    year: "2018",
+
+    title: "2018 作品展",
+
+    subtitle: "Exhibition 2018",
+
+    description:
+        "台灣國際拼布友好會 2018 年作品展紀錄。",
+
+    works: [
+
+        {
+            image: "/events/2018/work-01.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-02.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-03.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-04.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-05.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-06.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-07.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-08.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-09.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-10.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-11.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-12.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-13.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-14.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-15.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-16.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-17.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-18.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-19.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-20.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-21.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-22.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-23.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-24.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-25.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-26.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-27.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-28.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-29.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-30.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-31.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-32.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-33.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-34.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-35.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-36.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-37.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-38.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-39.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-40.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-41.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-42.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-43.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-44.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-45.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-46.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-47.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-48.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-49.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-50.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-051.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-52.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-53.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-54.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-55.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-56.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-57.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-58.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-59.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2018/work-60.jpg",
+            title: "",
+            artist: ""
+        }
+    ]
+};
