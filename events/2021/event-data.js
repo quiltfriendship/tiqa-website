2f2ee6fa5aa6ab6015643c91039e4b3d1e70254c
wenghuichen <wenghuichen@eleadernet.com>
2026-08-23 20:19:00 +0800

    EVENTS 2021 首頁

---
+++ events/2021/event-data.js
@@ -0,0 +1,108 @@
+/* =========================================================
+   TIQA 2021 友好拼布手作節
+   首頁 DATA
+   /events/2021/event-data.js
+========================================================= */
+
+const eventData = {
+
+    year: "2021",
+    title: "2021 友好拼布手作節",
+    subtitle: "在松菸 1 號倉庫",
+    englishTitle: "Taiwan Friendly Quilt & Craft Festival 2021",
+    image: "/events/2021/2021.jpg",
+
+    description:
+        "2021 友好拼布手作節於臺北松山文創園區一號倉庫舉辦，結合拼布作品展、徵件、市集展位、活動門票與志工招募等內容，呈現拼布創作、交流與產業串聯。",
+
+    facts: [
+        { label: "展出時間", value: "2021/11/12（五）－11/14（日）" },
+        { label: "活動地點", value: "臺北松山文創園區一號倉庫" },
+        { label: "主辦單位", value: "台灣國際拼布友好會" },
+        { label: "協辦單位", value: "臺灣喜佳股份有限公司" }
+    ],
+
+    exhibition: {
+        title: "作品展",
+        englishTitle: "Exhibition",
+        description:
+            "瀏覽 2021 友好拼布手作節展出的拼布作品，包含競賽、代表作、會員 50×50 與「萬紫千紅」主題作品等展覽紀錄。",
+        image: "/events/2021/exhibition.jpg",
+        url: "/events/2021/exhibition/"
+    },
+
+    sections: [
+        {
+            title: "關於活動",
+            englishTitle: "About",
+            description: "認識 2021 友好拼布手作節的活動內容與舉辦背景。",
+            url: "/events/2021/about/"
+        },
+        {
+            title: "最新消息",
+            englishTitle: "News",
+            description: "查看 2021 友好拼布手作節當年的活動公告與最新消息紀錄。",
+            url: "/events/2021/news/"
+        },
+        {
+            title: "活動門票",
+            englishTitle: "Tickets",
+            description: "查看 2021 活動門票、入場方式與相關歷史資訊。",
+            url: "/events/2021/ticket/"
+        },
+        {
+            title: "參展攤位介紹",
+            englishTitle: "Exhibitors",
+            description: "介紹「品牌廠商區」及「教室／工作室專區」參展攤位。",
+            url: "/events/2021/exhibitors/"
+        },
+        {
+            title: "展覽作品徵選",
+            englishTitle: "Competition",
+            description: "查看 2021 展覽作品徵選的報名資格、作品規範與歷史時程。",
+            date: "報名截止 2021/09/30｜作品寄達 2021/10/15 前",
+            url: "/events/2021/competition/"
+        },
+        {
+            title: "代表作徵件",
+            englishTitle: "Representative Works",
+            description: "查看 2021 代表作徵件的作品條件、報名與展出資訊。",
+            date: "報名截止 2021/09/30｜作品寄達 2021/10/15 前",
+            url: "/events/2021/representative/"
+        },
+        {
+            title: "會員專屬 50×50 作品徵召",
+            englishTitle: "Members 50×50",
+            description: "2021 台灣國際拼布友好會會員專屬 50×50 作品徵召。",
+            date: "報名截止 2021/10/15｜作品寄達 2021/10/31 前",
+            url: "/events/2021/member-quilt/"
+        },
+        {
+            title: "萬紫千紅－主題作品募集",
+            englishTitle: "Theme Quilt",
+            description: "查看 2021「萬紫千紅」主題作品募集與展出紀錄。",
+            date: "作品寄回 2021/08/10",
+            url: "/events/2021/theme-quilt/"
+        },
+        {
+            title: "徵求志工",
+            englishTitle: "Volunteers",
+            description: "查看 2021 友好拼布手作節志工招募與活動協力資訊。",
+            url: "/events/2021/volunteer/"
+        }
+    ],
+
+    englishSections: [
+        {
+            title: "About the Event",
+            description: "English introduction to the 2021 Taiwan Friendly Quilt & Craft Festival.",
+            url: "/events/2021/en/about/"
+        },
+        {
+            title: "Taiwan Friendly Quilt & Craft Festival 2021",
+            description: "English event information and archive.",
+            url: "/events/2021/en/"
+        }
+    ]
+
+};
---
+++ events/2021/exhibition/gallery-data.js
@@ -0,0 +1,213 @@
+const eventGalleryData = {
+    year: "2023",
+
+    title: "2023 作品展",
+
+    subtitle: "Exhibition 2023",
+
+    description:
+        "台灣國際拼布友好會 2023 年作品展紀錄。",
+
+    works: [
+
+        {
+            image: "/events/2023/exhibition/work-01.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2023/exhibition/work-02.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2023/exhibition/work-03.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2023/exhibition/work-04.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2023/exhibition/work-05.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2023/exhibition/work-06.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2023/exhibition/work-07.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2023/exhibition/work-08.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2023/exhibition/work-09.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2023/exhibition/work-10.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2023/exhibition/work-11.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2023/exhibition/work-12.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2023/exhibition/work-13.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2023/exhibition/work-14.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2023/exhibition/work-15.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2023/exhibition/work-16.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2023/exhibition/work-17.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2023/exhibition/work-18.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2023/exhibition/work-19.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2023/exhibition/work-20.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2023/exhibition/work-21.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2023/exhibition/work-22.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2023/exhibition/work-23.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2023/exhibition/work-24.jpg",
+            title: "",
+            artist: ""
+        },
+
+        {
+            image: "/events/2023/exhibition/work-25.jpg",
+            title: "",
+            artist: ""
+        },
+        {
+            image: "/events/2023/exhibition/work-26.jpg",
+            title: "",
+            artist: ""
+        },
+		{
+            image: "/events/2023/exhibition/work-27.jpg",
+            title: "",
+            artist: ""
+        },
+		{
+            image: "/events/2023/exhibition/work-28.jpg",
+            title: "",
+            artist: ""
+        },
+		{
+            image: "/events/2023/exhibition/work-29.jpg",
+            title: "",
+            artist: ""
+        },
+		{
+            image: "/events/2023/exhibition/work-30.jpg",
+            title: "",
+            artist: ""
+        },
+		{
+            image: "/events/2023/exhibition/work-31.jpg",
+            title: "",
+            artist: ""
+        },
+		{
+            image: "/events/2023/exhibition/work-32.jpg",
+            title: "",
+            artist: ""
+        },
+		{
+            image: "/events/2023/exhibition/work-33.jpg",
+            title: "",
+            artist: ""
+        },
+		{
+            image: "/events/2023/exhibition/work-34.jpg",
+            title: "",
+            artist: ""
+        },
+		{
+            image: "/events/2023/exhibition/work-35.jpg",
+            title: "",
+            artist: ""
+        }
+    ]
+};
