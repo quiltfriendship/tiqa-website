dc61c2e06e9ccf38f2ee907ca827aba84e6c9b0c
wenghuichen <wenghuichen@eleadernet.com>
2026-08-30 07:31:13 +0800

    2019 home

---
+++ events/2019/event-data.js
@@ -0,0 +1,106 @@
+const eventData = {
+    title: "2019 友好拼布手作節",
+    subtitle: "台北松山文創園區二號倉庫",
+    image: "/events/2019/2019.jpg",
+
+    description:
+        "第一屆友好拼布手作節於台北松山文創園區舉辦，結合拼布作品展、課程、作品徵件、台灣拼布大賽、市集及相關推廣活動。",
+
+    facts: [
+        {
+            label: "主辦單位",
+            value: "台灣國際拼布友好會"
+        },
+        {
+            label: "活動地點",
+            value: "台北市松山文創園區二號倉庫"
+        },
+        {
+            label: "課程時間",
+            value: "2019/12/10～12/15"
+        },
+        {
+            label: "展覽時間",
+            value: "2019/12/13～12/15"
+        },
+        {
+            label: "聯繫電話",
+            value: "0933-917555、0955-428566"
+        },
+        {
+            label: "電子信箱",
+            value: "pinbuzhan1768@gmail.com"
+        }
+    ],
+
+    exhibition: {
+        image: "/events/2019/exhibition.jpg",
+        description:
+            "瀏覽 2019 友好拼布手作節相關展覽作品與歷史活動紀錄。",
+        url: "/events/2019/exhibition/"
+    },
+
+    sections: [
+        {
+            englishTitle: "About",
+            title: "關於活動",
+            description: "2019 友好拼布手作節活動緣起、策展理念及相關活動說明。",
+            url: "/events/2019/about/"
+        },
+        {
+            englishTitle: "Tickets",
+            title: "活動門票",
+            description: "2019 友好拼布手作節活動門票及當年參觀資訊。",
+            url: "/events/2019/tickets/"
+        },
+        {
+            englishTitle: "Crowdfunding",
+            title: "築夢踏實－展覽作品集出版計畫",
+            description: "將展覽內容集結出版為作品集，紀錄專業作品、比賽作品、會員 50×50 作品及布展歷程。",
+            date: "募資期間：2019/11/11～12/15｜募集目標：NT$200,000",
+            url: "/events/2019/crowdfunding/"
+        },
+        {
+            englishTitle: "Workshops",
+            title: "名師工作坊",
+            description: "2019 友好拼布手作節名師課程及工作坊相關歷史資料。",
+            url: "/events/2019/workshops/"
+        },
+        {
+            englishTitle: "Members 50×50",
+            title: "會員專屬 50×50 徵召",
+            description: "2019 年會員專屬 50×50 拼布作品徵召資料。",
+            url: "/events/2019/member-quilt/"
+        },
+        {
+            englishTitle: "Representative Works",
+            title: "代表作徵件",
+            description: "2019 友好拼布手作節代表作徵件相關資料。",
+            url: "/events/2019/representative/"
+        },
+        {
+            englishTitle: "Theme Quilt",
+            title: "花現囍事－主題作品募集",
+            description: "集結近五百位拼布愛好者共同完成大型主題作品「花現囍事」。",
+            url: "/events/2019/theme-quilt/"
+        },
+        {
+            englishTitle: "Taiwan Quilt Competition",
+            title: "台灣拼布大賽",
+            description: "2019 友好拼布手作節台灣拼布大賽相關徵件及比賽資料。",
+            url: "/events/2019/competition/"
+        },
+        {
+            englishTitle: "Exhibitors",
+            title: "市集展位介紹",
+            description: "2019 友好拼布手作節市集參展單位與攤位相關資訊。",
+            url: "/events/2019/exhibitors/"
+        },
+        {
+            englishTitle: "Staff & Volunteers",
+            title: "策展工作小組及募集志工",
+            description: "2019 活動策展工作小組與志工募集相關歷史資料。",
+            url: "/events/2019/staff/"
+        }
+    ]
+};
