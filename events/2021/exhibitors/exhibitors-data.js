66e6a00789cc800a2f04016181961c156f766b1c
wenghuichen <wenghuichen@eleadernet.com>
2026-08-29 08:20:23 +0800

    2021 exhibitors

--- events/2021/exhibitors/exhibitors-data.js
+++ events/2021/exhibitors/exhibitors-data.js
@@ -3,104 +3,553 @@ window.exhibitors2021Data = {
     {
       "name": "立家手創館 RICCAR／伸興工業股份有限公司",
       "image": "brand-riccar.jpg",
-      "description": "全球家用縫紉機製造商，並以立家手創館 RICCAR 推廣縫紉、刺繡與手作文化。"
+      "description": [
+        "全球第一大縫紉機製造商，家用縫紉機每三台就有一台是伸興公司製造，53年歷史的伸興產品涵蓋全球各大洲，超過100多國，擁有完整家用縫紉機產品線；產品涵蓋低中高階機種。具備自主設計與研發的完整開發能力，近年來產品接連獲得國際設計大獎肯定。",
+        "而近年歐美國家縫紉、刺繡手作風潮盛行，為了能延續這股風氣，讓更多人參與手作文化，伸興創立「立家手創館 RICCAR」代理銷售 BERNINA、BERNETTE、NECCHI、RICCAR，從各個面向接觸使用者、傾聽市場聲音，期待最終回應消費者最佳的使用體驗。"
+      ],
+      "links": [
+        {
+          "label": "官網",
+          "url": "https://www.lovericcar.com/"
+        },
+        {
+          "label": "Facebook 粉絲專頁",
+          "url": "https://www.facebook.com/zh.riccar"
+        },
+        {
+          "label": "Instagram",
+          "url": "https://www.instagram.com/lovericcar/"
+        },
+        {
+          "label": "YouTube 頻道",
+          "url": "https://www.youtube.com/channel/UC06DO8tVlIsWSWqTaU8bCXQ"
+        },
+        {
+          "label": "立家縫紉刺繡小教室",
+          "url": "https://www.facebook.com/groups/167674687421401"
+        },
+        {
+          "label": "DRAWINGSXI 刺繡軟體",
+          "url": "https://www.facebook.com/groups/205216857965466"
+        },
+        {
+          "label": "2021 友好拼布手作節－獨家活動",
+          "url": "https://sites.google.com/view/riccar-special"
+        }
+      ]
     },
     {
       "name": "貝佳時尚布工坊",
       "image": "brand-beijia.jpg",
-      "description": "專營歐美、日、韓進口布、版權卡通布料，以及周邊配件與耗材。"
+      "address": "台北市延平北路一段140號",
+      "phone": "02-2559-5881",
+      "description": [
+        "我們擁有專業的團隊開發設計多種獨家布款！將布幻化為顏料，恣意揮灑色彩，創意無限！讓您耳目一新，顛覆布料的基本框架～期待您細細品味「貝佳」與眾不同的一面～",
+        "貝佳時尚布工坊專營歐美、日、韓進口布和版權卡通布料，及週邊配件和耗材，以最優惠的價格及最新的資料提供您最熱門的布品。"
+      ],
+      "links": [
+        {
+          "label": "Facebook 粉絲專頁",
+          "url": "https://www.facebook.com/%E8%B2%9D%E4%BD%B3%E6%99%82%E5%B0%9A%E5%B8%83%E5%B7%A5%E5%9D%8A-435007063271332/"
+        }
+      ]
     },
     {
       "name": "松芝有限公司",
       "image": "brand-matsushiba.jpg",
-      "description": "販售 JANOME 車樂美等縫紉、刺繡設備與軟體，並提供手作、洋裁、拼布教學及維修保養服務。"
+      "description": [
+        "專業販售 JANOME 車樂美等各品牌家用或商用之縫紉、刺繡、布邊、三本機器及軟體。",
+        "專業指導手作、洋裁、拼布各式縫紉教學。",
+        "販售縫紉用品、拼布材料、尺規工具及專用剪刀。",
+        "專精各式品牌縫紉和刺繡機器售後維修保養服務。"
+      ],
+      "contact": [
+        "土城門市｜236 新北市土城區清水路126號｜02-2262-4365",
+        "台北門市｜106 台北市大安區復興南路二段210巷3-1號｜02-2704-1808",
+        "高雄門市｜807 高雄市三民區博愛一路172號｜07-322-1733",
+        "LINE ID｜@janometw（車樂美官方旗艦店）"
+      ],
+      "special": [
+        "好友禮：送 3 支日製水消筆。",
+        "送購物金 $50（低消 $399），另有展場優惠方案。"
+      ],
+      "links": [
+        {
+          "label": "官網",
+          "url": "https://www.janome.com.tw/"
+        },
+        {
+          "label": "Facebook 粉絲專頁",
+          "url": "https://bit.ly/3A7C3Mj"
+        },
+        {
+          "label": "Instagram",
+          "url": "https://bit.ly/3uDi1bc"
+        },
+        {
+          "label": "YouTube 頻道",
+          "url": "https://bit.ly/3a5QSEo"
+        },
+        {
+          "label": "PChome 商店街",
+          "url": "https://www.pcstore.com.tw/janome"
+        },
+        {
+          "label": "PChome 24h",
+          "url": "https://24h.pchome.com.tw/store/DMAL9A"
+        },
+        {
+          "label": "蝦皮賣場",
+          "url": "https://shopee.tw/ying920624?categoryId=100010&itemId=100446124"
+        }
+      ]
     },
     {
       "name": "佳織縫紉有限公司",
       "image": "brand-kaori.jpg",
-      "description": "日本 JUKI 家用縫紉機臺灣總代理，推廣縫紉機、相關配件及縫紉手作周邊工具。"
+      "description": [
+        "佳織縫紉有限公司成立於 2014 年，是知名日本品牌 JUKI 家用縫紉機的臺灣總代理。JUKI（日語漢字為「重機」）成立於 1938 年，是日本知名工業用縫紉機製造商。承襲了專業縫製生產要求的高穩定性與精良的縫製品質，推出適合日常使用的家用縫紉機。",
+        "佳織致力於推廣 JUKI 縫紉機、相關配件及縫紉手作周邊工具，滿足每一位縫紉愛好者的需求。從商品比較、訂購體驗到售後服務，我們都會盡力提供最完善的服務。同時，也持續規劃各式活動及合作方式，以期讓更多朋友感受縫紉的美好。",
+        "我們期許自己不只提供商品的銷售，而是能讓大家體會到：縫紉可以是和生活息息相關的各種紡織品，可以是輕鬆紓壓的手作興趣，也可以是專業創作者揮灑創意的媒介——縫紉手作有無限可能。"
+      ],
+      "links": [
+        {
+          "label": "官網",
+          "url": "http://www.kaorihsm.com.tw/"
+        },
+        {
+          "label": "YouTube 頻道",
+          "url": "https://www.youtube.com/c/kaorihsm"
+        },
+        {
+          "label": "Facebook 粉絲專頁",
+          "url": "https://www.facebook.com/kaorihsm"
+        },
+        {
+          "label": "Instagram",
+          "url": "https://www.instagram.com/kaorihsm/"
+        },
+        {
+          "label": "LINE 官方帳號",
+          "url": "https://page.line.me/gpg6515z?openQrModal=true"
+        }
+      ]
     },
     {
       "name": "洋鈺生活有限公司",
       "image": "brand-yangyu.jpg",
-      "description": "自 2003 年起經營日本、美國布料、工具及歐美特殊線材，並持續推廣拼布文化交流。"
+      "description": [
+        "洋鈺生活有限公司成立於 2003 年，由第一代當家熟子阿嬤夫婦創立，初期進口日本、美國布料與相關工具為主，後期增加歐美特殊珍稀線材穩固布料生意根基。",
+        "2010 年突破傳統布商的買賣模式，首發集結數十位國內拼布藝者作品至韓國參展，將台灣拼布文化藝術提升至國際舞台，為拼布界增添更豐富的藝術交流。"
+      ],
+      "links": [
+        {
+          "label": "LINE 官方帳號",
+          "url": "https://line.me/R/ti/p/%40wfz7414o"
+        }
+      ]
     },
     {
       "name": "SED 信義町針車有限公司",
       "image": "brand-sed.jpg",
-      "description": "成立於 1963 年，開發縫紉機相關配件與壓腳，代理品牌縫紉機並提供售後服務。"
+      "description": [
+        "SED 信義町針車有限公司成立於 1963 年，SED 立志誠信服務，以開發研究為主軸服務客戶。長年開發出各種縫紉機相關配件與壓腳，代理各種品牌縫紉機販售，並以能為客戶做售後服務為基礎來販售產品。",
+        "在客戶與廠商雙贏的立場，極力為縫紉產業打造更有國際競爭力的市場。"
+      ],
+      "links": [
+        {
+          "label": "官網",
+          "url": "https://www.sed-sewing.com/"
+        },
+        {
+          "label": "YouTube 頻道",
+          "url": "https://www.youtube.com/channel/UCDzxZRnGTES2dA7DBEaXxGQ"
+        },
+        {
+          "label": "Facebook 粉絲專頁",
+          "url": "https://www.facebook.com/SEDSEWING"
+        },
+        {
+          "label": "Instagram",
+          "url": "https://www.instagram.com/sed_sewing/"
+        }
+      ]
     },
     {
       "name": "家合國際行銷股份有限公司",
       "image": "brand-jiahe.jpg",
-      "description": "代理 elna 縫紉機及各國縫紉工具、材料配件與主題布料，並規劃專業手作課程。"
+      "description": [
+        "家合國際行銷股份有限公司是由一個熱愛縫紉文創的團隊，以「勤、善、誠、信、精」的理念共同經營。",
+        "主要營業項目：代理 elna 縫紉機以及日本、韓國、美國、台灣等各國的縫紉工具、材料配件及主題布料的批發、零售及國際進出口業務。",
+        "同時為推展縫紉手作、拼布及洋裁的優質工藝，特與海內外各界名師共同合作，規劃各項專業教程，支援各項 B to B 教學活動以及 B to C 的基礎培養，期以凝聚許多不同的創意與想法，為縫紉的領域激盪出更多的「手作點子」。"
+      ],
+      "links": [
+        {
+          "label": "官網",
+          "url": "https://www.jhmission.com/"
+        },
+        {
+          "label": "Facebook 粉絲專頁",
+          "url": "https://www.facebook.com/jhelna/"
+        },
+        {
+          "label": "YouTube 頻道",
+          "url": "https://www.youtube.com/channel/UCnB4sT-T_BXFh0eKbiwPGrQ"
+        }
+      ]
     },
     {
       "name": "恭盟服飾材料股份有限公司",
       "image": "brand-gongmeng.jpg",
-      "description": "創立於 1989 年，專營服裝材料進口與批發，品項包含鈕釦、織帶、緞帶、蕾絲、扣具等。"
+      "description": [
+        "恭盟服飾材料股份有限公司創立於 1989 年，專營各式服裝材料進口及批發。產品多樣，品項眾多：鈕釦、織帶、緞帶、蕾絲、釦具……等，數萬種商品皆有現貨庫存。",
+        "1995 年與日本 MOKUBA（木馬）公司一同推廣「緞帶刺繡」手藝，並受到拼布業者的喜愛及廣泛運用。",
+        "代理品牌：日本 MOKUBA（木馬）、SHINDO（新道）、法國 SATAB、德國 GUNOLD／SUKLY 線等。註冊品牌：iChara 手創材料、依莉卡爾 Elizabeth Carole。",
+        "我們期許不論是一流服裝設計師或是拼布愛好者，都能運用恭盟的材料發掘更多創作靈感，創造出作品的獨一無二！"
+      ],
+      "links": [
+        {
+          "label": "官網",
+          "url": "http://www.kda.com.tw/"
+        },
+        {
+          "label": "Facebook 粉絲專頁",
+          "url": "https://www.facebook.com/Karomax.tw/"
+        },
+        {
+          "label": "YouTube 頻道",
+          "url": "https://www.youtube.com/user/kdatogether"
+        },
+        {
+          "label": "蝦皮商城",
+          "url": "https://shopee.tw/tailor.button"
+        },
+        {
+          "label": "PChome 商店街",
+          "url": "https://www.pcstore.com.tw/ichara/"
+        },
+        {
+          "label": "副料大學",
+          "url": "https://www.u-accss.com.tw/"
+        },
+        {
+          "label": "副料大學 Facebook",
+          "url": "https://www.facebook.com/U.Accss/"
+        }
+      ]
     },
     {
       "name": "隆德貿易有限公司／布能布玩拼布生活工坊",
       "image": "brand-longder.jpg",
-      "description": "專業拼布與彩繪手藝商品進口、批發及零售，代理多個國際手藝工具與布料品牌。"
+      "description": [
+        "隆德貿易有限公司／布能布玩拼布生活工坊，是專業拼布彩繪相關手藝商品進口批發零售商。秉持誠信，堅持最好的品質，進口優質拼布＆彩繪相關手藝用品。",
+        "代理日本可樂牌 Clover、富士克 Fujix、Olympus 製絲、HOBBYRA HOBBYRE、有輪商店、小林纖維、蝴蝶牌 Sewline、日本 Vilene、英國 LIBERTY FABRICS、美國 ROWAN、德國 Mettler、瑞典 TILDA、義大利 Aurifil 等國際知名手藝工具布料品牌，另有代理瑞士 BERNINA、美國 DecoArt 等其他手藝及彩繪用具共約 60 家品牌。",
+        "常聘請國內外彩繪＆拼布師資，教授專業手藝知識及技能，給台灣的手藝創作者更多元、更全面的手藝資源，發展豐富多彩的創意作品。",
+        "全省 3 家直營門市「布能布玩拼布工坊」，分布於台北、台中、高雄三地。"
+      ],
+      "links": [
+        {
+          "label": "官網",
+          "url": "http://www.patchworklife.com.tw/"
+        },
+        {
+          "label": "Facebook 粉絲專頁",
+          "url": "https://www.facebook.com/Longteh1997"
+        },
+        {
+          "label": "YouTube 頻道",
+          "url": "https://www.youtube.com/channel/UCE9v-gCio0MEo80-cR-Q9sw"
+        },
+        {
+          "label": "Instagram",
+          "url": "https://www.instagram.com/longteh_1994/"
+        },
+        {
+          "label": "2021 友好拼布手作節－獨家活動",
+          "url": "https://sites.google.com/view/longte2021"
+        }
+      ]
     },
     {
       "name": "陽鐘拼布飾品材料 DIY",
       "image": "brand-yangchung.jpg",
-      "description": "自 2008 年經營拼布飾品材料批發零售，並自行研發生產手工植鞣真皮提把。"
+      "description": [
+        "陽鐘拼布飾品材料 DIY 自 2008 年成立，主要批發、零售，產品多元化、快速掌握流行趨勢的市場區隔，以南臺灣拼布飾品最大賣場之姿立足於高雄三多商圈。",
+        "並成立皮革設計開發團隊，自行研發生產手工植鞣真皮提把，追求「品質」、「服務」之理念經營企業。"
+      ],
+      "special": [
+        "2021 友好拼布手作節－獨家活動：購物滿 $3000，贈送植鞣真皮小手機提把。"
+      ],
+      "links": [
+        {
+          "label": "Facebook 粉絲專頁",
+          "url": "https://www.facebook.com/ppplaydiy"
+        },
+        {
+          "label": "Instagram｜拼布玩",
+          "url": "https://www.instagram.com/sunbelldiy/"
+        },
+        {
+          "label": "蝦皮賣場",
+          "url": "https://shopee.tw/pp_playdiy?categoryId=73&itemId=1318935609&fbclid=IwAR1XTqUdBA-IOXKWO8t9zAmH0l_0pFhOHpML6fKUakKy35U-zxSGIOiZ994"
+        }
+      ]
     },
     {
       "name": "智廣實業有限公司",
       "image": "brand-zhiguang.jpg",
-      "description": "成立於 1981 年，經營 DMC 繡線、進口毛線、刺繡線及多種 DIY 手藝材料。"
+      "description": [
+        "智廣實業有限公司成立於 1981 年，專營拓展 DIY 手藝多年。",
+        "主要營業項目有：法國 DMC 繡線代理進口、歐洲進口毛線、加工手鉤紗線、十字繡線、刺繡線、切斷手鉤紗地毯、壓克力手鉤紗、竹篩十字繡等多種產品內外銷，也有學校家事教材及各種手藝材料。"
+      ],
+      "links": [
+        {
+          "label": "官網",
+          "url": "https://www.excelcraft.com.tw/"
+        },
+        {
+          "label": "LINE 官方帳號",
+          "url": "https://liff.line.me/1645278921-kWRPP32q/?accountId=lfj9602q"
+        },
+        {
+          "label": "ioCraft 我愛手藝",
+          "url": "https://www.iocraft.com.tw/"
+        },
+        {
+          "label": "Instagram",
+          "url": "https://www.instagram.com/yarncraft.addict/?utm_medium=copy_link"
+        },
+        {
+          "label": "YouTube 頻道",
+          "url": "https://www.youtube.com/channel/UCvMmsAuLlex5u46aKm4afbQ"
+        }
+      ]
     },
     {
       "name": "雅櫻手作鋪",
       "image": "brand-yaying.jpg",
-      "description": "由林雅櫻老師推廣拼布與縫紉教學，並經營 PFAFF 百福縫紉機及 SINGER 勝家縫紉機相關服務。"
+      "description": [
+        "林雅櫻老師於 1995 年開始教授拼布，在大學畢業後進入勝家縫紉機公司擔任才藝教學的工作，樂於分享心得及過程，深得學員喜愛。",
+        "1997 年成立雅櫻拼布藝術教室，目前則是德國 PFAFF 百福縫紉機台灣總代理及勝家 SINGER 縫紉機台中門市。"
+      ],
+      "links": [
+        {
+          "label": "雅櫻手作鋪－官網",
+          "url": "https://masasakurahandicraftmagazineoriginal.easy.co/"
+        },
+        {
+          "label": "Facebook｜雅櫻手作鋪",
+          "url": "https://www.facebook.com/%E9%9B%85%E6%AB%BB%E6%89%8B%E4%BD%9C%E8%88%96-206921206142882"
+        },
+        {
+          "label": "Facebook｜台灣百福縫紉機",
+          "url": "https://www.facebook.com/profile.php?id=100064060869312"
+        },
+        {
+          "label": "Mr. 勝家先生－YouTube",
+          "url": "https://www.youtube.com/channel/UCldHZufhAnRWUGJgvCYFVuw"
+        },
+        {
+          "label": "Instagram",
+          "url": "https://instagram.com/yaying_handmade?utm_medium=copy_link"
+        }
+      ]
     },
     {
       "name": "臺灣喜佳股份有限公司",
       "image": "brand-cheermall.jpg",
-      "description": "成立於 1993 年，為 brother 縫紉機／刺繡機臺灣地區總經銷，並創立 NCC 縫紉機品牌。"
+      "description": [
+        "臺灣喜佳股份有限公司成立於 1993 年，為日本第一品牌 brother 縫紉機／刺繡機之台灣地區總經銷，更創立了自有品牌 NCC 縫紉機。",
+        "以獨創的三合一經營理念持續推展縫紉休閒文化成為溫馨的縫紉休閒活動，透過專業的才藝教學來推展縫紉文化，精緻齊全的縫紉週邊產品來滿足客戶需求，品質優越、功能與機種齊全的縫紉機來供客戶選擇，讓縫紉機再度成為美化每一個家庭的神奇寶貝，也可以是熱愛藝術生活的另一支畫筆。",
+        "全台擁有 25 家直營縫紉生活館及專櫃外，還有約 400 家的經銷批發店、CheerMall 喜佳網購中心、大潤發量販店等通路，將縫紉全民化、普及化，期能擴大服務商圈、擴展更深層的縫紉領域，傳達縫紉的理念，帶領縫紉到文創的領域，將多彩多姿的縫紉才藝融入生活！"
+      ],
+      "links": [
+        {
+          "label": "官網",
+          "url": "http://www.cces.com.tw/"
+        },
+        {
+          "label": "Facebook 粉絲專頁",
+          "url": "https://www.facebook.com/cces.tw/"
+        },
+        {
+          "label": "CheerMall 喜佳網購中心",
+          "url": "https://www.cheermall.com.tw/"
+        },
+        {
+          "label": "Instagram",
+          "url": "https://www.instagram.com/cheermall/"
+        },
+        {
+          "label": "YouTube 頻道",
+          "url": "https://www.youtube.com/user/cheermall0301"
+        }
+      ]
     }
   ],
   "studios": [
     {
       "name": "一個小袋子工作室",
       "image": "studio-littlebag.jpg",
-      "description": "以夢想、創意與快樂為品牌精神，發展獨特手作包款與手作教學課程。"
+      "description": [
+        "一個小袋子工作室主理人李依宸老師說：有天看到一篇報導提到有一個全世界最快樂的地方叫「萬那杜」，那是一個很小的島國，人口只有二十幾萬人，但他們也需要教育，日常生活教育、各種常識都需要有人教育他們，但又不能用教條式的方法，所以當地就有一個劇團用戲劇的方式教育當地的人們，讓他們快樂學習又生活愉快，而那個劇團的名字就叫做「一個小袋子」。",
+        "人一生追求的不就是幸福與快樂嗎？有夢想就去追，做自己想做的事不也是幸福的嗎？如果可以把夢想、創意、快樂都裝進「一個小袋子」裡帶在身邊，那幸福與快樂不就也在身邊，於是「一個小袋子工作室」誕生了！"
+      ],
+      "special": [
+        "攤位打卡留言送拼布松樹印章（限量 15 份）或精選車線（限量 12 份）。",
+        "獨一無二手作包款，現場獨賣。",
+        "手作教學課程發表，現場報名享優惠價。"
+      ],
+      "links": [
+        {
+          "label": "官網",
+          "url": "http://www.asmallbag.com/"
+        },
+        {
+          "label": "Facebook",
+          "url": "https://www.facebook.com/profile.php?id=100001184470493"
+        },
+        {
+          "label": "YouTube 頻道",
+          "url": "https://www.youtube.com/channel/UC1nuBPtMT4WjoRyzacY52Ag"
+        },
+        {
+          "label": "Instagram",
+          "url": "https://www.instagram.com/dearlee200/"
+        }
+      ]
     },
     {
-      "name": "三色堇拼布坊",
+      "name": "三色菫拼布坊",
       "image": "studio-pansy.jpg",
-      "description": "由五位拼布愛好者共同創立，提供拼布材料、材料包、縫紉設備與多元拼布創意教學。"
+      "description": [
+        "三色菫拼布坊是由五位拼布的愛好者共同創立的，我們都很喜歡手作的感覺，既有 DIY 的樂趣，又可使一些廢物再利用。我們知道和我們一樣的同好有很多，但在大環境之中，似乎少了些什麼，我們不想追隨流行，也不完全哈日；五人當中有四位是媽媽，我們了解媽媽們的需要，經過一陣子的摸索，最後決定成立網路商店來服務同好！",
+        "三色菫拼布坊提供的服務有：拼布材料、拼布材料包、縫紉機、刺繡機販售，拼布創意教學、拼布興趣班、拼布創作指導、自由曲線、馬賽克拼布教學、永漢系統手縫證書、永漢系統機縫證書、禮物拼被等。"
+      ],
+      "special": [
+        "購物滿千送精美小禮物。"
+      ],
+      "links": [
+        {
+          "label": "Blog 部落格",
+          "url": "https://blog.xuite.net/pansy_5sisters"
+        },
+        {
+          "label": "Facebook 粉絲專頁",
+          "url": "https://www.facebook.com/%E4%B8%89%E8%89%B2%E8%8F%AB%E6%8B%BC%E5%B8%83%E5%9D%8A-%E4%B8%89%E8%89%B2%E5%A0%87%E6%8B%BC%E5%B8%83%E5%9D%8A-234835586601114"
+        }
+      ]
     },
     {
       "name": "川久屋拼布藝術工作室",
       "image": "studio-kawakoya.jpg",
-      "description": "藍玉娟老師於 1998 年成立，以手作拼布創造浪漫生活，持續投入拼布藝術教學。"
+      "address": "彰化市實踐路66號",
+      "phone": "04-7285319",
+      "description": [
+        "藍玉娟老師為日本文部科學省許可授權光喬台灣第四屆拼布講師，在 1998 年成立川久屋拼布藝術工作室。",
+        "揮灑浪漫心情，創造優質生活……是自己對手作拼布單純不變的想法，也是持續至今的信念及力量。"
+      ],
+      "links": [
+        {
+          "label": "Facebook 粉絲專頁",
+          "url": "https://www.facebook.com/%E5%B7%9D%E4%B9%85%E5%B1%8B%E6%8B%BC%E5%B8%83%E8%97%9D%E8%A1%93%E5%B7%A5%E4%BD%9C%E5%AE%A4-713648078660632/"
+        }
+      ]
     },
     {
       "name": "小野布房拼布教室",
       "image": "studio-ono.jpg",
-      "description": "成立於 2007 年，提供證書認證、手縫、機縫及簡易洋裁等課程。"
+      "line": "@ufj4107u",
+      "description": [
+        "小野布房拼布教室成立於 2007 年，色彩繽紛的印花布、各種各樣的縫線，源源不斷地激發我們的創作能量，不遺餘力地追求美感，堅持藝術本身的純粹性，也延續著一貫簡單、舒適且有型的風格，不受時間、空間的影響或考驗。",
+        "課程包含證書認證班、手縫課程、機縫課程及簡易洋裁，有適合無經驗朋友的客製化入門課程及適合有經驗朋友的進階研修課程，在官方網站上都有針對課程的介紹。"
+      ],
+      "links": [
+        {
+          "label": "官網",
+          "url": "https://linshouse0.gogoshopapp.com/?fbclid=IwAR2W6YM_ZXyxbZ97k4YG5zEdCXLnHKzd7afNoa8nAKw682h4mwtgZKkEA3k"
+        },
+        {
+          "label": "Facebook 粉絲專頁",
+          "url": "https://www.facebook.com/%E5%B0%8F%E9%87%8E%E5%B8%83%E6%88%BF%E6%8B%BC%E5%B8%83%E6%95%99%E5%AE%A4-186940174681428/"
+        }
+      ]
     },
     {
       "name": "木棉拼布美學藝坊",
       "image": "studio-kapok.jpg",
-      "description": "成立於約 1990 年，提供拼布藝術交流、專業教學、進口材料及客製手作品。"
+      "address": "基隆市仁愛區孝四路24號2樓",
+      "phone": "02-24293917",
+      "description": [
+        "木棉拼布美學藝坊成立於約 1990 年。",
+        "拼布藝術交流－專業教學園地。",
+        "進口布料、書籍、工具、材料包。",
+        "拼布作品及客製手作品販售。"
+      ],
+      "special": [
+        "滿千可抵 50 元抵用券一張。",
+        "滿 2000 再送精美小禮物。"
+      ],
+      "links": [
+        {
+          "label": "Facebook 粉絲專頁",
+          "url": "https://www.facebook.com/profile.php?id=100057533556271"
+        }
+      ]
     },
     {
       "name": "快樂屋拼布才藝坊",
       "image": "studio-happyhouse.jpg",
-      "description": "位於台北文山區，經營超過 22 年，以拼布教學為主，並販售布料、工具、材料及書籍。"
+      "address": "台北市文山區木新路三段215巷3號",
+      "phone": "02-22343025、0911-839621",
+      "description": [
+        "快樂屋拼布才藝坊是位在文山區經營超過 22 年的拼布教室。",
+        "教室負責人張碧恩老師不論手縫、機縫皆有指導員資歷，教學經驗豐富，國內外拼布展獲獎、邀展無數次。",
+        "教室營業項目主要以教學為主，另有多種美日布料、工具、材料、配件、書籍、材料包皆有販售，歡迎來店參觀選購。"
+      ],
+      "links": [
+        {
+          "label": "Facebook",
+          "url": "https://www.facebook.com/joy.house.116/"
+        }
+      ]
     },
     {
       "name": "拼布花園",
       "image": "studio-quiltgarden.jpg",
-      "description": "龎慧如老師的拼布教室，以「發現拼布新樂趣」為願景，作品風格自然、活潑、亮麗。"
+      "description": [
+        "「拼布花園」是龎慧如老師的教室名稱，「發現拼布新樂趣」則是龎老師的願景。",
+        "龎老師從 1999 年開始學習拼布，多年來在作品的設計和製作上受到歐美拼布的影響很多，選色大膽、自然、活潑、亮麗。製作工法偏好不規則裁切和拼接，讓每個線條都有不同的變化。",
+        "在教學部分則喜歡刺激學生動腦筋，建立學生自己的風格，帶領大家進入拼布更深的境界，同時也善長使用邏輯的方式教導學生。龎老師的作品常上各大拼布雜誌和國際比賽，目前也在 YouTube 上公開分享一些作品經驗。"
+      ],
+      "special": [
+        "11/14（日）上午 9:30 大會活動舞台教學分享。",
+        "主題：「彩繪玻璃拼布作法和奇異襯的使用」。",
+        "如何使用黑色邊條製作出彩繪玻璃的效果。",
+        "如何使用奇異襯讓作品增加更多變化。"
+      ],
+      "links": [
+        {
+          "label": "官網",
+          "url": "https://www.patchworkgarden.com.tw/"
+        },
+        {
+          "label": "Facebook",
+          "url": "https://www.facebook.com/Vickys.Patchwork.Garden/"
+        },
+        {
+          "label": "YouTube 頻道",
+          "url": "https://www.youtube.com/c/VickysPatchworkGarden"
+        },
+        {
+          "label": "Instagram",
+          "url": "https://www.instagram.com/vickys_patchworkgarden/"
+        },
+        {
+          "label": "部落格",
+          "url": "https://pbhy.blogspot.com/"
+        }
+      ]
     }
   ]
 };
--- events/2021/exhibitors/exhibitors.js
+++ events/2021/exhibitors/exhibitors.js
@@ -3,48 +3,132 @@
 
     var data = window.exhibitors2021Data || {};
 
+    function addParagraphs(container, paragraphs) {
+        if (!Array.isArray(paragraphs)) return;
+
+        paragraphs.forEach(function (text) {
+            var p = document.createElement("p");
+            p.textContent = text;
+            container.appendChild(p);
+        });
+    }
+
+    function addContact(container, item) {
+        var rows = [];
+
+        if (item.address) rows.push(["地址", item.address]);
+        if (item.phone) rows.push(["電話", item.phone]);
+        if (item.line) rows.push(["LINE ID", item.line]);
+
+        if (Array.isArray(item.contact)) {
+            item.contact.forEach(function (text) {
+                rows.push(["", text]);
+            });
+        }
+
+        if (!rows.length) return;
+
+        var box = document.createElement("div");
+        box.className = "exhibitors2021-contact";
+
+        rows.forEach(function (row) {
+            var div = document.createElement("div");
+
+            if (row[0]) {
+                var strong = document.createElement("strong");
+                strong.textContent = row[0];
+                div.appendChild(strong);
+            }
+
+            var span = document.createElement("span");
+            span.textContent = row[1];
+            div.appendChild(span);
+
+            box.appendChild(div);
+        });
+
+        container.appendChild(box);
+    }
+
+    function addSpecial(container, items) {
+        if (!Array.isArray(items) || !items.length) return;
+
+        var box = document.createElement("div");
+        box.className = "exhibitors2021-special";
+
+        var title = document.createElement("h4");
+        title.textContent = "2021 友好拼布手作節－獨家活動";
+        box.appendChild(title);
+
+        var ul = document.createElement("ul");
+
+        items.forEach(function (text) {
+            var li = document.createElement("li");
+            li.textContent = text;
+            ul.appendChild(li);
+        });
+
+        box.appendChild(ul);
+        container.appendChild(box);
+    }
+
+    function addLinks(container, links) {
+        if (!Array.isArray(links) || !links.length) return;
+
+        var wrap = document.createElement("div");
+        wrap.className = "exhibitors2021-links";
+
+        links.forEach(function (item) {
+            var a = document.createElement("a");
+            a.href = item.url;
+            a.target = "_blank";
+            a.rel = "noopener";
+            a.textContent = item.label;
+            wrap.appendChild(a);
+        });
+
+        container.appendChild(wrap);
+    }
+
     function renderCards(containerId, items, type) {
         var container = document.getElementById(containerId);
 
-        if (!container || !Array.isArray(items)) {
-            return;
-        }
+        if (!container || !Array.isArray(items)) return;
 
         items.forEach(function (item) {
             var article = document.createElement("article");
-            article.className = "exhibitors2021-card " + (
-                type === "brand"
-                    ? "exhibitors2021-card-brand"
-                    : "exhibitors2021-card-studio"
-            );
+            article.className =
+                "exhibitors2021-card exhibitors2021-card-" + type;
 
-            var imageLink = document.createElement("a");
-            imageLink.className = "exhibitors2021-image";
-            imageLink.href = "./" + item.image;
-            imageLink.target = "_blank";
-            imageLink.rel = "noopener";
+            var media = document.createElement("div");
+            media.className = "exhibitors2021-media";
 
-            var image = document.createElement("img");
-            image.src = "./" + item.image;
-            image.alt = item.name || "";
-            image.loading = "lazy";
+            var img = document.createElement("img");
+            img.src = "./" + item.image;
+            img.alt = item.name || "";
+            img.loading = "lazy";
 
-            imageLink.appendChild(image);
+            media.appendChild(img);
 
-            var body = document.createElement("div");
-            body.className = "exhibitors2021-card-body";
+            var content = document.createElement("div");
+            content.className = "exhibitors2021-content";
 
             var title = document.createElement("h3");
             title.textContent = item.name || "";
+            content.appendChild(title);
 
-            var description = document.createElement("p");
-            description.textContent = item.description || "";
+            addContact(content, item);
 
-            body.appendChild(title);
-            body.appendChild(description);
+            var description = document.createElement("div");
+            description.className = "exhibitors2021-description";
+            addParagraphs(description, item.description);
+            content.appendChild(description);
 
-            article.appendChild(imageLink);
-            article.appendChild(body);
+            addSpecial(content, item.special);
+            addLinks(content, item.links);
+
+            article.appendChild(media);
+            article.appendChild(content);
 
             container.appendChild(article);
         });
