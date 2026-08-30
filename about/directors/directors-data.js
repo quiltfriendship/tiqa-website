afc1c0df8dba736efbe739bad7e704aa132361e7
wenghuichen <wenghuichen@eleadernet.com>
2026-08-30 22:04:39 +0800

    directory

--- about/directors/directors-data.js
+++ about/directors/directors-data.js
@@ -4,7 +4,7 @@ const directorsData = [
         group: "主要幹部",
         position: "理事長",
         name: "伍麗華",
-        image: "/assets/images/directors/wu-li-hua.jpg",
+        image: "./wu-li-hua.jpg",
         facebook: "https://www.facebook.com/profile.php?id=100001698492091"
     },
 
@@ -12,7 +12,7 @@ const directorsData = [
         group: "主要幹部",
         position: "秘書長",
         name: "郭芷廷",
-        image: "/assets/images/directors/kuo-chih-ting.jpg",
+        image: "./kuo-chih-ting.jpg",
         facebook: "https://www.facebook.com/quiltwork"
     },
 
@@ -20,95 +20,88 @@ const directorsData = [
         group: "理事",
         position: "常務理事",
         name: "賴芳慧",
-        image: "/assets/images/directors/lai-fang-hui.jpg",
+        image: "./lai-fang-hui.jpg",
         facebook: "https://www.facebook.com/profile.php?id=100004020961248"
     },
-
-    {
-        group: "監事",
-        position: "常務監事",
-        name: "龎慧如",
-        image: "/assets/images/directors/pang-hui-ju.jpg",
-        facebook: "https://www.facebook.com/share/12KsHrWzoH9/?mibextid=wwXIfr"
-    },
-
-    {
+	{
         group: "理事",
         position: "常務理事",
         name: "洪藝芳",
-        image: "/assets/images/directors/hung-yi-fang.jpg",
+        image: "./hung-yi-fang.jpg",
         facebook: "https://www.facebook.com/profile.php?id=100057533556271"
     },
-
     {
         group: "監事",
-        position: "監事",
+        position: "常務監事",
         name: "林美鈴",
-        image: "/assets/images/directors/lin-mei-ling.jpg",
+        image: "./lin-mei-ling.jpg",
         facebook: "https://www.facebook.com/happinesspisces"
-    },
-
+    },    
     {
         group: "監事",
         position: "監事",
         name: "歐秀桂",
-        image: "/assets/images/directors/ou-hsiu-kuei.jpg",
+        image: "./ou-hsiu-kuei.jpg",
         facebook: "https://www.facebook.com/profile.php?id=100042699664694"
     },
-
+    {
+        group: "監事",
+        position: "監事",
+        name: "李易娟",
+        image: "./li-yi-chuan.jpg",
+        facebook: "https://www.facebook.com/gigi.lee.39"
+    },
     {
         group: "理事",
         position: "理事",
         name: "王梨香",
-        image: "/assets/images/directors/wang-li-hsiang.jpg",
+        image: "./wang-li-hsiang.jpg",
         facebook: "https://www.facebook.com/profile.php?id=61561240952750"
     },
-
     {
         group: "理事",
         position: "理事",
-        name: "吳玉婷",
-        image: "/assets/images/directors/wu-yu-ting.jpg",
-        facebook: "https://www.facebook.com/kity888"
-    },
-
-    {
-        group: "理事",
-        position: "理事",
-        name: "劉玉琳",
-        image: "/assets/images/directors/liu-yu-lin.jpg",
+        name: "劉霂薽",
+        image: "./liu-yu-lin.jpg",
         facebook: "https://www.facebook.com/profile.php?id=100000160375395"
     },
-
     {
         group: "理事",
         position: "理事",
-        name: "林淑娟",
-        image: "/assets/images/directors/lin-shu-chuan.jpg",
-        facebook: "https://www.facebook.com/joselina.lin"
+        name: "熊嘉玲",
+        image: "./default.jpg",
+        facebook: ""
     },
 
     {
         group: "理事",
         position: "理事",
-        name: "李易娟",
-        image: "/assets/images/directors/li-yi-chuan.jpg",
-        facebook: "https://www.facebook.com/gigi.lee.39"
+        name: "陳頤",
+        image: "./default.jpg",
+        facebook: ""
     },
 
     {
         group: "理事",
         position: "理事",
-        name: "林雅櫻",
-        image: "/assets/images/directors/lin-ya-ying.jpg",
-        facebook: "https://www.facebook.com/profile.php?id=100063996245336&mibextid=LQQJ4d"
+        name: "林婉真",
+        image: "./default.jpg",
+        facebook: ""
+    },
+
+    {
+        group: "理事",
+        position: "理事",
+        name: "連小慈",
+        image: "./default.jpg",
+        facebook: ""
     },
 
     {
         group: "行政",
         position: "會計",
         name: "黃麗靜",
-        image: "/assets/images/directors/huang-li-ching.jpg",
+        image: "./huang-li-ching.jpg",
         facebook: "https://www.facebook.com/profile.php?id=100000192093046"
     }
 
