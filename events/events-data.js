381f62afd77103e60899ef9d44bb5a7ea134dfef
wenghuichen <wenghuichen@eleadernet.com>
2026-09-07 00:32:28 +0800

    fix events error

--- events/events-data.js
+++ events/events-data.js
@@ -1,75 +1,130 @@
+/* =========================================================
+   TIQA 歷年活動資料
+========================================================= */
+
 const eventsData = [
 
     {
         year: "2026",
-        title: "2026 曬被節｜第九屆曬被節",
+
+        title: "2026 曬被節",
+
         description:
             "2026 第九屆曬被節以「拼布走進大稻埕」為主題，串聯拼布創作、街區文化、手作交流與公共空間。",
-        image: "/events/2026/hero.jpg",
-        url: "/events/2026/"
+
+        image:
+            "/events/2026.jpg",
+
+        url:
+            "/events/2026/"
+    },{
+        year: "2025",
+
+        title: "2025 友好拼布手作節",
+
+        description:
+            "2025 年友好拼布手作節活動紀錄，包含活動介紹、展覽、工作坊及相關資訊。",
+
+        image:
+            "/events/2025.jpg",
+
+        url:
+            "/events/2025/"
     },
 
-    {
-        year: "2025",
-        title: "2025 友好拼布手作節",
-        description:
-            "2025 友好拼布手作節以「青春之歌」為主題，結合拼布作品展、競賽、工作坊及創藝市集。",
-        image: "/events/2025/2025.jpg",
-        url: "/events/2025/"
-    },
 
     {
         year: "2023",
+
         title: "2023 友好拼布手作節",
+
         description:
-            "2023 友好拼布手作節以「美好時光」為主題，回顧展覽、創作及相關交流活動。",
-        image: "/events/2023/2023.jpg",
-        url: "/events/2023/"
+            "2023 年友好拼布手作節活動紀錄，回顧展覽、創作及相關交流活動。",
+
+        image:
+            "/events/2023.jpg",
+
+        url:
+            "/events/2023/"
     },
+
 
     {
         year: "2021",
+
         title: "2021 友好拼布手作節",
+
         description:
-            "2021 友好拼布手作節包含拼布展覽、作品徵件、市集展位及相關活動紀錄。",
-        image: "/events/2021/2021.jpg",
-        url: "/events/2021/"
+            "2021 年友好拼布手作節活動紀錄，保存當年度的拼布創作與活動內容。",
+
+        image:
+            "/events/2021.jpg",
+
+        url:
+            "/events/2021/"
     },
+
 
     {
         year: "2019",
+
         title: "2019 友好拼布手作節",
+
         description:
-            "第一屆友好拼布手作節，包含展覽、競賽、工作坊、市集及國際交流活動。",
-        image: "/events/2019/2019.jpg",
-        url: "/events/2019/"
+            "第一屆友好拼布手作節，包含展覽、競賽、工作坊、市集及策展相關紀錄。",
+
+        image:
+            "/events/2019.jpg",
+
+        url:
+            "/events/2019/"
     },
 
     {
         year: "2018",
+
         title: "2018 展覽作品",
+
         description:
             "回顧 2018 年展覽作品，保存台灣國際拼布友好會早期的創作與展覽紀錄。",
-        image: "/events/2018/2018.jpg",
-        url: "/events/2018/"
+
+        image:
+            "/events/2018.jpg",
+
+        url:
+            "/events/2018/"
     },
+
 
     {
         year: "2017",
+
         title: "2017 展覽作品",
+
         description:
             "回顧 2017 年展覽作品，保存台灣國際拼布友好會早期的創作與展覽紀錄。",
-        image: "/events/2017/2017.jpg",
-        url: "/events/2017/"
+
+        image:
+            "/events/2017.jpg",
+
+        url:
+            "/events/2017/"
     },
+
 
     {
         year: "2016",
+
         title: "2016 展覽作品",
+
         description:
             "回顧 2016 年展覽作品，保存台灣國際拼布友好會早期的創作與展覽紀錄。",
-        image: "/events/2016/2016.jpg",
-        url: "/events/2016/"
+
+        image:
+            "/events/2016.jpg",
+
+        url:
+            "/events/2016/"
     }
 
 ];
